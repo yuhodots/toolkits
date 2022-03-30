e2902ecb15edc1fe8aa2a7b80c14b11cdf1ee3b3
Yuho Jeong <yuho8437@unist.ac.kr>
2022-03-31 01:29:49 +0900

    feat: Initialize parse module implementation

---
+++ toolkits/parse/between_lines.py
@@ -0,0 +1,19 @@
+"""
+Extract the log between the two input sentences
+"""
+
+
+def file2list():
+    pass
+
+
+def between_lines():
+    pass
+
+
+def between_lines_on_file():
+    pass
+
+
+def between_lines_on_dir():
+    pass
